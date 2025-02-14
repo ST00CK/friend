e78adc4a7f73b21f461a1cb5b49112b470d074bc
BMDevOps <51756458+BMPDevOps@users.noreply.github.com>
2025-02-14 14:35:18 +0900

    ENV 설정 변경

--- config/config.go
+++ config/config.go
@@ -2,14 +2,20 @@ package config
 
 import (
 	"github.com/joho/godotenv"
-	"log"
 	"os"
 )
 
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf(".env 에러: %v", err)
+	if godotenv.Load() != nil {
+		GetEnv("NEO4J_URI")
+		GetEnv("NEO4J_USERNAME")
+		GetEnv("NEO4J_PASSWORD")
+		GetEnv("APP_PORT")
+	} else {
+		err := godotenv.Load()
+		if err != nil {
+			println("Error loading .env file")
+		}
 	}
 }
 
